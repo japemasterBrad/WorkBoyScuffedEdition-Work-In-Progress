71eb20c44ac4c254fa73c2b1910cb280206d3a6f
Bradley Thomas <bradley.thomas60@googlemail.com>
2021-03-06 20:48:11 +0000

    Added starting code to convert temp from C to F

--- WorkBoyScuffedEdition/Program.cs
+++ WorkBoyScuffedEdition/Program.cs
@@ -4,10 +4,11 @@ namespace WorkBoyScuffedEdition
 {
     class Program
     {
-        public static Temperature temp;
+        public static Temperature temp = ;
+
         static void Main(string[] args)
         {
-            
+            temp.ConvertTemp(30);
         }
     }
 }
--- WorkBoyScuffedEdition/Temperature.cs
+++ WorkBoyScuffedEdition/Temperature.cs
@@ -6,6 +6,22 @@ namespace WorkBoyScuffedEdition
 {
     class Temperature
     {
+        public int TempCelcius { get; set; }
 
+        
+        public Temperature(int tempCelcius)
+        {
+            TempCelcius = tempCelcius;
+        }
+        public static void ConvertTemp(int tempCelcius)
+        {
+            Console.WriteLine("What do you want to convert from F to C?");
+            tempCelcius = Console.Read();
+
+            Console.WriteLine("So you want to convert {0} from Celcius to Farenheit.\n\n", tempCelcius);
+            Console.WriteLine("TEST FOR NEW LINE");
+
+        }
+        
     }
 }
