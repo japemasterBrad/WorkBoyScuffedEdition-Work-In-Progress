9801f47aadabd00db07868b8f1ee26cde1d7e27b
Bradley Thomas <bradley.thomas60@googlemail.com>
2021-03-06 23:46:09 +0000

    Adding functionality to convert from either temperature

--- WorkBoyScuffedEdition/Program.cs
+++ WorkBoyScuffedEdition/Program.cs
@@ -5,26 +5,89 @@ namespace WorkBoyScuffedEdition
     class Program
     {
         public static Temperature temperature = new Temperature();
+        public static bool running = true;
+        public static string workboyFunctionSelector;
+        public static string workboyRunAgain;
 
         static void Main(string[] args)
         {
-            bool running = true;
+            FunctionSelector();
+        }
+
+        # region FUNCTION SELECTOR.
+
+        public static void FunctionSelector()
+        {
+            Console.WriteLine("What do you want to do?\n" +
+                "1) Tempertaure");
+            workboyFunctionSelector = Console.ReadLine();
 
             do
             {
-                Console.WriteLine("What do you want to convert from C to F?");
-                string CelciusReading = Console.ReadLine();
+                if (workboyFunctionSelector.Equals("1"))
+                {
+                    TemperatureConverter();
+                }
 
-                int ParsedCelciusReading = Convert.ToInt32(CelciusReading);
+                RunAgain();
 
-                temperature.ConvertCtoF(ParsedCelciusReading);
+            } while (running);
+
+            if (workboyFunctionSelector.Equals("1"))
+            {
+                TemperatureConverter();
+            }
+        }
+
+        #endregion
+
+        #region RunAgain
+
+        public static void RunAgain()
+        {
+            Console.WriteLine("Do you want to do anythig else? (y/n)");
+            workboyRunAgain = Console.ReadLine();
+
+            if (workboyFunctionSelector.Equals("y")) running = true;
+            else if (workboyFunctionSelector.Equals("n")) running = false;
+        }
+        #endregion
+
+        public static void TemperatureConverter()
+        {
+            bool temperatureRunning = true;
+            do
+            {
+                Console.WriteLine("Do you want to convert from Celcius to Farenheit," +
+                    " or Farenheit to Celcius?\n" +
+                    "1) C to F\n" +
+                    "2) F to C");
+
+                string tempConversionPicker = Console.ReadLine();
+
+                if (tempConversionPicker.Equals("1"))
+                {
+                    Console.WriteLine("What Celcius temperature do you want to convert to Farenheit?");
+                    string celciusReading = Console.ReadLine();
+
+                    float ParsedCelciusReading = Convert.ToInt32(celciusReading);
+
+                    temperature.ConvertCtoF(ParsedCelciusReading);
+                }
+                else if (tempConversionPicker.Equals("2"))
+                {
+                    Console.WriteLine("What Farenheit temperature do you want to convert to Celcius?");
+                    string farenheitReading = Console.ReadLine();
+
+                    int ParsedFarenheitReading = Convert.ToInt32(farenheitReading);
+
+                    temperature.ConvertFtoC(ParsedFarenheitReading);
+                }
+
                 Console.WriteLine("Convert another temperature? (Y/N)");
                 string runAgain = Console.ReadLine();
 
-                if (runAgain.Equals("Y") || runAgain.Equals("y")) running = true;
-                else running = false;
-
-            } while (running == true);
+            } while (temperatureRunning = true);
         }
     }
 }
--- WorkBoyScuffedEdition/Temperature.cs
+++ WorkBoyScuffedEdition/Temperature.cs
@@ -6,13 +6,23 @@ namespace WorkBoyScuffedEdition
 {
     public class Temperature
     {
-        public int tempCelcius { get; set; }
+        public float tempCelcius;
+        public float tempFarenheit;
 
-        public void ConvertCtoF(int tempCelcius)
+        public void ConvertCtoF(float tempCelcius)
         {
-            int tempFarenheit = tempCelcius * 9 / 5 + 32;
+            float ConvertedTempFarenheit = tempCelcius * 9 / 5 + 32;
 
             Console.WriteLine("{0}F is equal to {1}C", tempCelcius, tempFarenheit);
+            Console.ReadKey();
+        }
+
+        public void ConvertFtoC(float tempFarenheit)
+        {
+            float ConvertedFtoC = (tempFarenheit - 32) * 5/9;
+           
+            Console.WriteLine("{0}F is equal to {1}C", tempFarenheit, ConvertedFtoC);
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE");
             Console.ReadKey();
         }
     }
